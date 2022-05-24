3dd6c7ca6c160673ce5dc6d3ee846ed2c2174819
iiHerilo <armargumball@gmail.com>
2022-05-24 15:27:31 -0500

    Make Enemy AI Script

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    public int health = 2;
+    public float speed = 4.0f;
+    public string PlayerName = "Player";
+
+    Vector2 direction;
+    Rigidbody2D physics;
+    GameObject Player;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        physics = GetComponent<Rigidbody2D>();
+        Player = GameObject.FindGameObjectWithTag("Player");
+        Debug.Log(Player);
     }
 
     // Update is called once per frame
     void Update()
     {
+        float theta = Mathf.Atan((transform.transform.position.y - Player.transform.position.y) / (transform.position.x - Player.transform.position.x));
+        if(transform.position.x - Player.transform.position.x <= 0) 
+            theta += Mathf.PI;
+        Vector2 velocity = new Vector2(Mathf.Cos(theta) * speed, Mathf.Sin(theta) * speed);
+        physics.velocity = -velocity;
         
     }
 }
