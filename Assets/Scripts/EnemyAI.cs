6823f185d03dec6303c62fb42b3e027c8021b341
iiHerilo <armargumball@gmail.com>
2022-05-24 15:51:14 -0500

    Add Enemy Sight Range

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -6,11 +6,12 @@ public class EnemyAI : MonoBehaviour
 {
     public int health = 2;
     public float speed = 4.0f;
-    public string PlayerName = "Player";
+    public float sightRange = 15f;
 
-    Vector2 direction;
     Rigidbody2D physics;
     GameObject Player;
+
+    bool active = false;
 
 
     // Start is called before the first frame update
@@ -18,17 +19,26 @@ public class EnemyAI : MonoBehaviour
     {
         physics = GetComponent<Rigidbody2D>();
         Player = GameObject.FindGameObjectWithTag("Player");
-        Debug.Log(Player);
     }
 
     // Update is called once per frame
     void Update()
     {
-        float theta = Mathf.Atan((transform.transform.position.y - Player.transform.position.y) / (transform.position.x - Player.transform.position.x));
-        if(transform.position.x - Player.transform.position.x <= 0) 
-            theta += Mathf.PI;
-        Vector2 velocity = new Vector2(Mathf.Cos(theta) * speed, Mathf.Sin(theta) * speed);
-        physics.velocity = -velocity;
+        Vector2 diffs = new Vector2((transform.position.x - Player.transform.position.x), (transform.transform.position.y - Player.transform.position.y));
+        // Enemy only moves if player is within a certain range
+        if(Mathf.Abs(Mathf.Sqrt((diffs.x * diffs.x) + (diffs.y * diffs.y))) <= sightRange) {
+            float theta = Mathf.Atan(diffs.y / diffs.x);
+            // For some reason omitting this causes the enemy to run in the wrong direction
+            if(diffs.x <= 0) 
+                theta += Mathf.PI;
+            Vector2 velocity = new Vector2(Mathf.Cos(theta) * speed, Mathf.Sin(theta) * speed);
+            physics.velocity = -velocity;
+        }
         
+        
+    }
+
+    void ActivateAI() {
+
     }
 }
