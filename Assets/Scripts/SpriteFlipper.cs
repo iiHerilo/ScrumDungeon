84fc4e8b341abc511034a27f594a9ef2e9d17f93
elindesmith <100791013+elindesmith@users.noreply.github.com>
2022-05-27 13:36:21 -0500

    flip sword with player

--- Assets/Scripts/SpriteFlipper.cs
+++ Assets/Scripts/SpriteFlipper.cs
@@ -7,6 +7,7 @@ public class SpriteFlipper : MonoBehaviour
     Vector2 last;
     bool flipped = false;
     public bool isPlayer = false;
+    public bool isSword = false;
     void Start() {
         last = transform.position;
     }
@@ -16,13 +17,31 @@ public class SpriteFlipper : MonoBehaviour
         float input = Input.GetAxis("Horizontal");
         float difference = last.x - transform.position.x;
         //Debug.Log(string.Format("Player: {0}\nInput: {1}\n Last X:"))
-        if(((isPlayer && input < 0) ^ (!isPlayer && difference > 0)) && !flipped) {
-            GetComponent<SpriteRenderer>().flipX = true;
+        if((((isPlayer || isSword) && input < 0) ^ (!isPlayer && !isSword && difference > 0)) && !flipped) {
+            if (!isSword)
+            {
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
             flipped = true;
+            if (isSword)
+            {
+                GetComponent<SpriteRenderer>().flipY = true;
+                Vector2 position = transform.position;
+                position.x = -0.5f;
+            }
         }
-        else if(((isPlayer && input > 0) ^ (!isPlayer && difference < 0)) && flipped) {
-            GetComponent<SpriteRenderer>().flipX = false;
-            flipped = false;
+        else if((((isPlayer || isSword) && input > 0) ^ (!isPlayer && !isSword && difference < 0)) && flipped) {
+            if (!isSword)
+            {
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
+                flipped = false;
+            if (isSword)
+            {
+                GetComponent<SpriteRenderer>().flipY = false;
+                Vector2 position = transform.position;
+                position.x = 0.5f;
+            }
         }
         last = transform.position;
     }
